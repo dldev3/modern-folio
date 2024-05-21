06cf3536a7bbe97351cc43a092e2405de79ae38d
dlshnkLK <dilshankaliyanage@yahoo.com>
2024-05-21 13:14:34 +0530

    ui components devs

---
+++ components/Grid.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BentoGrid } from './ui/BentoGrid'
+
+const Grid = () => {
+    return (
+        <section id="about">
+           
+        </section>
+    )
+}
+
+export default Grid

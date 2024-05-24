f55cd80a641ebd7e8d3a59510bc5f74a504be91c
dlshnkLK <dilshankaliyanage@yahoo.com>
2024-05-24 20:55:36 +0530

    bento fixed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,6 +52,10 @@ export const BentoGridItem = ({
             }}
         >
 
+            <div className={`${id === 6} && 'flex justify-center h-full' `}>
+
+            </div>
+
             <div className="group-hover/bento:translate-x-2 transition duration-200">
 
                 <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
